10f615281452b68988b415b8f438d7d44051594d
srg <srg@codeserg.io>
2023-09-20 17:24:59 -0600

    added pages/links to the header

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,27 +1,42 @@
-import '../globals.css';
+import { getPages } from '@/sanity/sanity-utils';
 import type { Metadata } from 'next';
 import Link from 'next/link';
+import '../globals.css';
 
 export const metadata: Metadata = {
   title: 'codeSerg Sample',
   description: 'Sample code showcase. Generated with Next.js and Sanity.io',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // get all pages
+  const pages = await getPages();
+
   return (
     <html lang="en">
       <body className="max-w-3xl mx-auto py-10">
-        <header>
+        <header className="flex items-center justify-between">
           <Link
             href="/"
             className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold"
           >
-            codeSerg
+            codeSerg logo
           </Link>
+          <div className="flex items-center gap-5 text-sm text-gray-600">
+            {pages.map((page) => (
+              <Link
+                key={page._id}
+                href={`/${page.slug}`}
+                className="hover:underline"
+              >
+                {page.title}
+              </Link>
+            ))}
+          </div>
         </header>
         <main className="py-20">{children}</main>
       </body>
